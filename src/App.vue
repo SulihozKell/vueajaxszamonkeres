92951e35f725003c26ee8c657313661c8647d9a9
Nagy Bendegúz <bendeguz@medea.hu>
2022-02-08 14:42:22 +0100

    Statues adatok betöltése táblázatba.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <thead>
+        <tr>
+          <th>Személy</th>
+          <th>Magasság</th>
+          <th>Ár</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="statue in statues" v-bind:key="statue.id">
+          <td>{{ statue.person }}</td>
+          <td>{{ statue.height }}</td>
+          <td>{{ statue.price }}</td>
+          <td>
+            <button @click="deleteStatue(statue.id)">Törlés</button>
+            <button @click="editStatue(statue.id)">Szerkesztés</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      statue: {
+        id: null,
+        person: '',
+        height: 0,
+        price: 0
+      },
+      statues: []
+    }
+  },
+  methods: {
+    async loadData () {
+      let Response = await fetch('http://127.0.0.1:8000/api/statues')
+      let data = await Response.json()
+      this.statues = data
+    }
+  },
+  mounted() {
+    this.loadData()
   }
 }
 </script>
